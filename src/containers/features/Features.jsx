70759fc90ae0968e94257e76d41e152716e513d3
Aditya_Prasad <adityaprasad186@gmail.com>
2021-12-02 18:09:43 +0530

    added ui features

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './features.css';
+const Features = () => {
+    return (
+        <div>
+            <p>Feature</p>
+        </div>
+    )
+}
+
+export default Features
