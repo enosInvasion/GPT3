1a7a67105e5095ef811a647ec0b2c88b25abe71b
Aditya_Prasad <adityaprasad186@gmail.com>
2021-12-04 21:18:17 +0530

    added possibility and cta

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./article.css";
 
-const Article = () => {
+const Article = ({imgUrl}) => {
     return (
-        <div>
-            
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imgUrl} alt=""/>
+            </div>
         </div>
     )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './blog.css';
+import { Article } from "../../components"
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
+
 const Blog = () => {
     return (
-        <div>
+        <div className="gpt3__blog" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">
+                    A lot is happening we are blogging about it.
+                </h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container_groupA">
+                    <Article imgUrl={blog01} date="Sept 26, 2021"/>
+                </div>
+                <div className="gpt3__blog-container_groupB">
+                    <Article imgUrl={blog02} date="Sept 26, 2021"/>
+                    <Article imgUrl={blog03} date="Sept 26, 2021"/>
+                    <Article imgUrl={blog04} date="Sept 26, 2021"/>
+                    <Article imgUrl={blog05} date="Sept 26, 2021"/>
+                </div>
+            </div>
             
         </div>
     )
